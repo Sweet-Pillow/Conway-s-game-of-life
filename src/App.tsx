6c8738b43cd71e1cddbe18d4ad071c26d5501a1a
Sweet-Pillow <daniel_h.m@outlook.com>
2025-06-19 18:44:19 -0300

    feat: button functions to create the generations

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,8 @@ const ROWS = 10
 
 export default function App() {
   const [matrix, setMatrix] = useState<number[]>(new Array(COLS * ROWS).fill(0))
-  const [isRunning, setIsRunning] = useState<boolean>(false)
+  
+  const newMatrix = new Array(COLS * ROWS).fill(0)
 
   return (
     <main className='w-screen h-screen flex justify-center items-center flex-col gap-6'>
@@ -19,27 +20,79 @@ export default function App() {
       />
       <Settings 
         run={run}
-        stop={stop}
         clear={clear}
-        onStep={onStep}
-        isRunning={isRunning}
       />
     </main>
   )
 
   function run() {
-    setIsRunning(true)
-  }
 
-  function stop() {
-    setIsRunning(false)
+    steps()
+
+    setMatrix(newMatrix)
+
   }
 
   function clear() {
-
+    newMatrix.map(() => (0))
+    setMatrix(newMatrix)
   }
 
-  function onStep() {
+  function steps() {
+    
+    for(let pos = 0; pos < matrix.length; pos++){
+      // if(matrix[pos] !== 0) continue
+
+      const x = Math.floor(pos/COLS)
+      const y = pos % COLS
+      const yLeft = (x * COLS) + y - 1
+      const yRight = (x * COLS) + y + 1
+      const xTopLeftCorner = x - 1 < 0? -5: ((x - 1) * COLS) + y - 1
+      const xBottomLeftCorner = ((x + 1) * COLS) + y - 1
+      
+      let neighbours = 0
+
+      for(let i = 0; i < 3; i++) {
+        if(matrix[xTopLeftCorner + i] === 1 && (xTopLeftCorner + i) >= 0 && (xTopLeftCorner + i) <= (COLS * ( x + 2 ))) {
+          neighbours ++
+        }
+      }
+
+      for(let i = 0; i < 3; i++) {
+        if(matrix[xBottomLeftCorner + i] === 1 && (xBottomLeftCorner + i) >= 0 && (xBottomLeftCorner + i) <= (COLS * ( x + 2 ))) {
+          neighbours ++
+        }
+      }
+
+      if(yLeft >= 0 && matrix[yLeft] === 1) {
+        neighbours ++
+      }
+
+      if(yRight < (COLS * ( x + 2 )) && matrix[yRight] === 1) {
+        neighbours ++
+      }
+
+      // if(neighbours < 2) {
+      //   newMatrix[pos] = 0
+      // } else {
+      //   newMatrix[pos] = 1
+      // }
+
+      if(matrix[pos] === 0) {
+        if(neighbours === 3) {
+          newMatrix[pos] = 1
+        }
+        continue
+      }
+
+      if(matrix[pos] === 1) {
+        if(neighbours < 2 || neighbours > 3) {
+          newMatrix[pos] = 0
+        } else {
+          newMatrix[pos] = 1
+        }
+      }
+    }
 
   }
 }
--- src/components/Settings/index.tsx
+++ src/components/Settings/index.tsx
@@ -2,32 +2,22 @@ import Button from "./Button";
 
 interface props {
   run: () => void
-  stop: () => void
   clear: () => void
-  onStep: () => void
-  isRunning: boolean
 }
 
 export default function Settings({
   run,
-  stop,
   clear,
-  onStep,
-  isRunning,
 }: props) {
   return(
     <div className='flex flex-row gap-9'>
-      <Button 
-        text={isRunning? 'Stop': 'Run'}
-        onClick={isRunning? stop: run}
+      <Button
+        text={'Run'}
+        onClick={run}
       />
       <Button 
         text={'Clear'}
         onClick={clear}
-      />
-      <Button 
-        text={'One step'}
-        onClick={onStep}
       />
     </div>
   )
